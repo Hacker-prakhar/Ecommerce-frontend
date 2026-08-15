fe5774fa8954c09f86e66e7cc66e8ed7251b571c
PRAKHAR KUMAR <kumarprakhar73@gmail.com>
2026-08-15 19:21:24 +0530

    Refactor route structure for checkout and confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,14 @@ function App() {
           <Route path='/contact' element={ <Contact />}/>
           <Route path='/cart' element={ <Cart />}/>
         
-          <Route path='/' element={<PrivateRoute />}>
-            <Route path='/checkout' element={ <Checkout />}/>
-            <Route path='/order-confirm' element={ <PaymentConfirmation />}/>
-          </Route>
+<Route path='/' element={<PrivateRoute />}> 
+    <Route path='/checkout' element={<Checkout />}/>
+</Route>
+
+<Route
+    path='/order-confirm'
+    element={<PaymentConfirmation />}
+/>
 
           <Route path='/' element={<PrivateRoute publicPage />}>
             <Route path='/login' element={ <LogIn />}/>
